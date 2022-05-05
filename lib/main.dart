06992250c1faff0f1e8578a5e90fbf82a250e2ab
CSAURABH <578saurabh@gmail.com>
2022-05-05 16:19:51 +0530

    modified lib/main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -13,78 +13,207 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.grey[100],
-        body: Column(
-          children: [
-            Container(
-              height: 300,
-              width: double.infinity,
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage("assets/images/background.jpg"),
-                  fit: BoxFit.cover,
-                ),
-              ),
-              child: Container(
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.bottomRight,
-                    colors: [
-                      Colors.black.withOpacity(0.6),
-                      Colors.black.withOpacity(0.1),
-                    ],
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                height: 300,
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/images/background.jpg"),
+                    fit: BoxFit.cover,
                   ),
                 ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    const Text(
-                      "What would you like to find?",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
+                child: Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.bottomRight,
+                      colors: [
+                        Colors.black.withOpacity(0.8),
+                        Colors.black.withOpacity(0.1),
+                      ],
                     ),
-                    const SizedBox(
-                      height: 30.0,
-                    ),
-                    Container(
-                      padding: const EdgeInsets.symmetric(
-                        vertical: 5.0,
-                        horizontal: 5,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      const Text(
+                        "What would you like to find?",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
                       ),
-                      margin: const EdgeInsets.symmetric(horizontal: 40.0),
-                      height: 60.0,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(50.0),
+                      const SizedBox(
+                        height: 30.0,
                       ),
-                      child: TextFormField(
-                        decoration: const InputDecoration(
-                          border: InputBorder.none,
-                          hintText: "Search for cities, places...",
-                          hintStyle: TextStyle(
-                            color: Colors.grey,
-                            fontSize: 20.0,
-                          ),
-                          prefixIcon: Icon(
-                            Icons.search,
-                            color: Colors.grey,
+                      Container(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 5.0,
+                          horizontal: 5,
+                        ),
+                        margin: const EdgeInsets.symmetric(horizontal: 40.0),
+                        height: 60.0,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(50.0),
+                        ),
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "Search for cities, places...",
+                            hintStyle: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 20.0,
+                            ),
+                            prefixIcon: Icon(
+                              Icons.search,
+                              color: Colors.grey,
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    const SizedBox(
-                      height: 30.0,
-                    ),
-                  ],
+                      const SizedBox(
+                        height: 30.0,
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            )
-          ],
+              const SizedBox(
+                height: 30.0,
+              ),
+              SizedBox(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Best Destination",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey[800],
+                          fontSize: 20.0,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 15.0,
+                      ),
+                      SizedBox(
+                        height: 200,
+                        child: ListView(
+                          scrollDirection: Axis.horizontal,
+                          children: [
+                            itemCard(
+                                image: "assets/images/canada.jpg",
+                                title: "Canada"),
+                            itemCard(
+                                image: "assets/images/Italy.jpg",
+                                title: "Italy"),
+                            itemCard(
+                                image: "assets/images/greece.jpg",
+                                title: "Greece"),
+                            itemCard(
+                                image: "assets/images/united-states.jpg",
+                                title: "United States"),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 30.0,
+                      ),
+                      SizedBox(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Best Hotels",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.grey[800],
+                                fontSize: 20.0,
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 15.0,
+                            ),
+                            SizedBox(
+                              height: 200,
+                              child: ListView(
+                                scrollDirection: Axis.horizontal,
+                                children: [
+                                  itemCard(
+                                      image: "assets/images/united-states.jpg",
+                                      title: "Canada"),
+                                  itemCard(
+                                      image: "assets/images/greece.jpg",
+                                      title: "Italy"),
+                                  itemCard(
+                                      image: "assets/images/canada.jpg",
+                                      title: "Greece"),
+                                  itemCard(
+                                      image: "assets/images/Italy.jpg",
+                                      title: "United States"),
+                                ],
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 30.0,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
+}
+
+Widget itemCard({image, title}) {
+  return AspectRatio(
+    aspectRatio: 1 / 1,
+    child: Container(
+      margin: const EdgeInsets.only(right: 15.0),
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(image),
+          fit: BoxFit.cover,
+        ),
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+      child: Container(
+        padding: const EdgeInsets.all(20.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20.0),
+          gradient: LinearGradient(
+            begin: Alignment.bottomRight,
+            colors: [
+              Colors.black.withOpacity(0.8),
+              Colors.black.withOpacity(0.2),
+            ],
+          ),
+        ),
+        child: Align(
+          alignment: Alignment.bottomLeft,
+          child: Text(
+            title,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 20.0,
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
 }
